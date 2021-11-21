6f532f7998df0d4b62486dbc26e9a19825630ded
Tanvir Shakil <tanvirshakil777@gmail.com>
2021-11-21 14:09:12 +0600

    Next.js and tailwind css ui design Navbar and Banner part

---
+++ components/Home/HomePage.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      {/* Red Onion Banner */}
+      <div
+        className={`bg-header-banner bg-[#FCF4E0] h-[450px] bg-contain bg-center flex flex-col items-center justify-center bg-no-repeat`}
+      >
+        <h1 className="font-semibold text-gray-600 text-xl -mt-10 lg:-mt-0 lg:text-5xl">
+          Best Food Waiting For Your Belly
+        </h1>
+        <form className="mt-8 items-center">
+          <input
+            className="px-5 py-2 rounded-full focus:outline-none"
+            type="text"
+            placeholder="Search food items"
+          />
+          <button
+            className="bg-red-500 px-7 py-2 rounded-full -ml-10  text-white"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "header-banner":
+          "url('https://i.ibb.co/GnYBX6d/banner-background.png')",
+      }),
+    },
   },
   variants: {
-    extend: {},
+    backgroundImage: ["responsive", "hover", "focus"],
   },
   plugins: [],
-}
+};
